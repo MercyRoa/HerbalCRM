95d6cd951091b92465319b881cd7fef709ed5191
Mercy Roa <mercy@grupoinnovait.com>
2012-08-11 19:21:28 -0500

    empty rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerbalCRM::Application.config.secret_token = '2a2455d2490919658c3166b9a004b2ea512ef94f99e5fad482938b0be196810dfb1481d6423bb5bd7f5597b27afce9ab3ca24af8459d000b37c45c15e5810cad'
