a038f106a8b6c7bffc7e0b571916ee57c0a5ec3c
Arnold Roa <manolet@gmail.com>
2012-11-10 13:30:21 -0500

    fix to mail sequence

--- app/models/mail_sequence.rb
+++ app/models/mail_sequence.rb
@@ -1,4 +1,9 @@
 class MailSequence < ActiveRecord::Base
   belongs_to :campaign
   validates_presence_of :step, :subject
+  before_save :set_plain_from_html
+
+  def set_plain_from_html
+    self.body_text = html_to_text(body_html)
+  end
 end
