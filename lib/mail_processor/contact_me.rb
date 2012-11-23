bc9e63637df0c0381a6eb4a2ae80da1be03d8156
Arnold Roa <manolet@gmail.com>
2012-11-23 08:31:15 -0500

    fix to contact me mail processor

--- lib/mail_processor/contact_me.rb
+++ lib/mail_processor/contact_me.rb
@@ -5,13 +5,12 @@ module MailProcessor
     def self.identify message, email
       if ADDRESS.include? message.from then
         puts '-- ContactMe ---'
+        message.body = message.body.match(/(-{10,})(.*?)\1/m)[2].gsub!(/^\s+/, ' ') + " --CM"
         email.from.first.name = message.body.match(/Nombre: (.*)/)[1].strip
         message.from = message.body.match(/Email: (.*)/)[1].strip
         message.subject = "Hola #{email.from.first.name}"
-        message.body = message.body.match(/(-{10,})(.*?)\1/m)[2].squish + " --CM"
       end
       return message, email
     end
   end
 end
-
