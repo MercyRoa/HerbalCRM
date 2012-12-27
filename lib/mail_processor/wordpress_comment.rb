4ca0875e7f50e4cac7a953a48ae94faf6b9a5f7d
Arnold Roa <manolet@gmail.com>
2012-12-27 12:48:33 -0500

    FINALLY FINAL FIX

--- lib/mail_processor/wordpress_comment.rb
+++ lib/mail_processor/wordpress_comment.rb
@@ -11,7 +11,7 @@ module MailProcessor
         #Change campaign?
 
         #message.body = message.body.match(/(-{10,})(.*?)\1/m)[2].gsub!(/^\s+/, ' ') + " --CM"
-        email.from.first.name = message.body.match(/Autor: ([\w ]*) \(/)[1].strip
+        email.from.first.name = message.body.match(/Autor : ([\w ]*) \(/)[1].strip
         message.from = message.body.match(/Correo electrónico : (.*)/)[1].strip
         message.subject = "Hola #{email.from.first.name}"
       end
