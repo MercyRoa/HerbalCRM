6a07040c9bd26c93d5a18a0fc83bf7073b60d4c8
Arnold Roa <manolet@gmail.com>
2013-09-17 14:41:50 -0500

    Added processor for unbounce

---
+++ lib/mail_processor/unbounce.rb
@@ -0,0 +1,15 @@
+module MailProcessor
+  class ContactMe
+    ADDRESS = ['notifications@unbounce.com']
+
+    def self.identify message, email
+      if ADDRESS.include? message.from then
+        puts '-- Unbounce ---'
+        email.from.first.name = message.body.match(/\*nombre\* (.*)/)[1].strip
+        message.from = message.body.match(/mailto:([^"]*)"/)[1].strip
+        message.subject = "Hola #{email.from.first.name}! "
+      end
+      return message, email
+    end
+  end
+end
