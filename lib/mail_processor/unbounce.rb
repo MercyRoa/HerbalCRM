ec0c0bba9729df3ec78935b7728a86ec186aa66f
Arnold Roa <manolet@gmail.com>
2013-09-17 15:26:38 -0500

    Added processor for unbounce

--- lib/mail_processor/unbounce.rb
+++ lib/mail_processor/unbounce.rb
@@ -6,7 +6,7 @@ module MailProcessor
       if ADDRESS.include? message.from then
         puts '-- Unbounce ---'
         email.from.first.name = message.body.match(/nombre: (.*)/)[1].strip
-        message.from = message.body.match(/email: (.*)"/)[1].strip
+        message.from = message.body.match(/email: (.*)/)[1].strip
         message.subject = "Hola #{email.from.first.name}! "
       end
       return message, email
