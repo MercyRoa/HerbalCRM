41dc7c683fd116ea43422c1ef7bef034119799b9
Arnold Roa <manolet@gmail.com>
2014-10-22 22:07:36 -0500

    removing ssl mail

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Turn off auto TLS for e-mail
+ActionMailer::Base.smtp_settings[:openssl_verify_mode] = false
+ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
+
 # Initialize the rails application
 HerbalCRM::Application.initialize!
